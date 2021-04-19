8e75acccb1f259a5b1026dc3ec0d2b4b0d2dce74
jrb28 <james.bradley@mason.wm.edu>
2021-04-19 08:00:42 -0400

    Add files via upload

--- matmul.py
+++ matmul.py
@@ -7,15 +7,14 @@ Created on Fri Apr 16 11:07:27 2021
 
 import numpy as np
 import multiprocessing as mp
-
+from numba import njit
 
 if __name__ == '__main__':
     n = 10
-
     p = np.random.random((n,n))
     q = np.random.random((n,n))
     
     print(p.dot(q))
     
-''' create code below using numpy and multiprocessing to replicate dot product above '''
-
+''' create code below using numpy, multiprocessing, and numba to replicate dot product above '''
+'''   - a function may be required '''
